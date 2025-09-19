f71ade277ab5117b93861390cf2fc940a6c3c834
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2025-09-19 16:10:08 +0700

    Layout Logic: Login First auth logic added, directed to Home

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,9 @@
+import { useColorScheme } from '@/hooks/use-color-scheme';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useState } from 'react';
+import LoginScreen from './login';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -11,12 +11,24 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  if (!isAuthenticated) {
+    return (
+      <>
+        <LoginScreen
+          onLoginSuccess={() => setIsAuthenticated(true)}
+        />
+        <StatusBar style='auto' />
+      </>
+    );
+  }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
+      <Stack initialRouteName='(tabs)'>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        {/* <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} /> */}
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
