871eed2e0f62b33970a338c2ca97bbef2d13c8c7
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2025-09-20 15:56:44 +0700

    conditional exception for web bundle test added

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,19 +1,27 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
+import { Platform } from 'react-native';
 import 'react-native-url-polyfill/auto';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_KEY;
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase URL or Anon Key in environment variables');
 }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+let options: Parameters<typeof createClient>[2] = {
   auth: {
-    storage: AsyncStorage,
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
   },
-});
+};
+
+// Only use AsyncStorage on native, not web
+if (Platform.OS !== 'web') {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const AsyncStorage = require('@react-native-async-storage/async-storage').default;
+  options.storage = AsyncStorage;
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, options);
