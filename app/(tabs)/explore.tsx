6f2ba38b0ac50a082e270fa5cc9484efb85774e7
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2025-09-20 15:59:22 +0700

    Logout button supabase integrated added

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,13 +1,13 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
-import { Collapsible } from '@/components/ui/collapsible';
 import { ExternalLink } from '@/components/external-link';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
+import { Collapsible } from '@/components/ui/collapsible';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Fonts } from '@/constants/theme';
+import { supabase } from '@/utils/supabase';
+import { Image } from 'expo-image';
+import { Button, Platform, StyleSheet } from 'react-native';
 
 export default function TabTwoScreen() {
   return (
@@ -94,6 +94,7 @@ export default function TabTwoScreen() {
           ),
         })}
       </Collapsible>
+      <Button title='Logout' onPress={async () => { await supabase.auth.signOut(); }} />
     </ParallaxScrollView>
   );
 }
