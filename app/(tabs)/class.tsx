084fa3da015b712737d1e73448d042f5b9c21cef
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2025-10-05 16:27:03 +0700

    Initiall mock up

---
+++ app/(tabs)/class.tsx
@@ -0,0 +1,20 @@
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+import { StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function ClassroomScreen() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <ThemedView style={styles.container}>
+        <ThemedText type='title' style={styles.title}>Classroom</ThemedText>
+      </ThemedView>
+    </SafeAreaView>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 16, gap: 8 },
+  title: { marginBottom: 8, textAlign: 'center' },
+});
